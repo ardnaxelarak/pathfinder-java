426dbaa3c5013988b32f8dcdbccbfd3069e43d1b
Zachary J Felix <zf2153@messiah.edu>
2015-01-14 00:19:44 -0500

    Added text area for outputting messages

--- src/pathfinder/gui/MainDisplay.java
+++ src/pathfinder/gui/MainDisplay.java
@@ -17,14 +17,15 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 public class MainDisplay extends JFrame implements KeyListener, EncounterListener
 {
-	private JScrollPane scrollPane;
 	private CharacterDisplay chdisp;
 	private OrderedCharacters characters;
 	private InputStatus instat = InputStatus.DISABLED;
 	private Character current;
+	private JTextArea messages;
 	public MainDisplay()
 	{
 		super("Pathfinder");
@@ -34,12 +35,27 @@ public class MainDisplay extends JFrame implements KeyListener, EncounterListene
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		characters = new OrderedCharacters();
 		characters.addListener(this);
+
 		chdisp = new CharacterDisplay(characters);
-		scrollPane = new JScrollPane(chdisp,
+		JScrollPane chPane = new JScrollPane(chdisp,
 			JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
 			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setBorder(BorderFactory.createEmptyBorder());
-		getContentPane().add(scrollPane, BorderLayout.EAST);
+		chPane.setBorder(BorderFactory.createEmptyBorder());
+
+		messages = new JTextArea(5, 80);
+		JScrollPane mePane = new JScrollPane(messages,
+			JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
+			JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		mePane.setBorder(BorderFactory.createEmptyBorder());
+		messages.setEditable(false);
+		messages.setLineWrap(true);
+		messages.setWrapStyleWord(true);
+
+		getContentPane().add(chPane, BorderLayout.LINE_END);
+		JPanel left = new JPanel(new BorderLayout(0, 0));
+		left.add(messages, BorderLayout.PAGE_START);
+		getContentPane().add(left, BorderLayout.CENTER);
+
 		pack();
 		setVisible(true);
 		addKeyListener(this);
